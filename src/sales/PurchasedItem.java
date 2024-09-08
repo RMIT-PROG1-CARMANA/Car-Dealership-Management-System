a85416012167574d307f2c44e67db97a0ee89070
Tram Anh <170843226+peachiejeje@users.noreply.github.com>
2024-09-08 22:38:05 +0700

    Create PurchasedItem.java

---
+++ src/sales/PurchasedItem.java
@@ -0,0 +1,40 @@
+package sales;
+
+import part.AutoPart;
+import vehicle.Car;
+
+public class PurchasedItem {
+    private Car car;
+    private AutoPart part;
+
+    // Constructor, getters, setters
+
+    public PurchasedItem(Car car, AutoPart part) {
+        this.car = car;
+        this.part = part;
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
+
+    public AutoPart getPart() {
+        return part;
+    }
+
+    public void setPart(AutoPart part) {
+        this.part = part;
+    }
+
+    @Override
+    public String toString() {
+        return "PurchasedItem {" +
+                "car=" + (car != null ? car.toString() : "No car purchased") +
+                ", part=" + (part != null ? part.toString() : "No part purchased") +
+                '}';
+    }
+}
